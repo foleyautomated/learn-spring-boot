010b3dd4e0002151e594260b606c716cd0e5a029
David Foley <foleyautomated@gmail.com>
2024-06-18 12:26:06 -0400

    BrokenBuild

--- 01-spring-boot-overview/01-springbootdemo/src/main/java/com/example/mycoolapp/rest/FunRestController.java
+++ 01-spring-boot-overview/01-springbootdemo/src/main/java/com/example/mycoolapp/rest/FunRestController.java
@@ -10,7 +10,7 @@ public class FunRestController {
 
     @GetMapping("/")
     public String sayHello() {
-        return "Hello World!";
+        return "Hello World!!!!!!!!!!";
     }
 
 }
---
+++ 01-spring-boot-overview/02-devtoolsdemo/src/main/java/com/example/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,16 @@
+package com.example.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+    // expose "/" that returns 'Hwllo World'
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!!!!!!!!!!";
+    }
+
+}
